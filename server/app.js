4780f45b5a03bdf41cfdaa23df1e0a9af22f48d3
Rohit Rao <rohit@rohitsrao.com>
2023-06-12 15:29:46 +0200

    Setup graphql endpoint

--- server/app.js
+++ server/app.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const graphqlHTTP = require('express-graphql').graphqlHTTP;
 
 const app = express();
 
+app.use('/graphql', graphqlHTTP({
+
+}));
+
 app.listen(4000, () => {
-  console.log('Now listening for requests on port 4000.')
+  console.log('Now listening for requests on port 4000.');
 })
