b4e34ae93fa7b79faa1fc39101b8428ac225e8f8
Rohit Rao <rohit@rohitsrao.com>
2023-06-12 16:35:36 +0200

    Can resolve and return data from dummy data

--- server/app.js
+++ server/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql').graphqlHTTP;
+const schema = require('./schema/schema');
+
 
 const app = express();
 
 app.use('/graphql', graphqlHTTP({
-
+  schema: schema,
+  graphiql: true,
 }));
 
 app.listen(4000, () => {
