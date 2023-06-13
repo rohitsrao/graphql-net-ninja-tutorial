b3d60fd21f1305251327a92bef5ae18d912cd838
Rohit Rao <rohit@rohitsrao.com>
2023-06-13 08:50:44 +0200

    Created Schemas for Book and Author for MongoDB

--- server/app.js
+++ server/app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql').graphqlHTTP;
 const schema = require('./schema/schema');
-
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://rohit:rw9rWL32Pn7H19fk@cluster0.88dmr3o.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connection.once('open', () => {
+  console.log('Connected to MongoDB Atlas instance.');
+});
 
 app.use('/graphql', graphqlHTTP({
   schema: schema,
